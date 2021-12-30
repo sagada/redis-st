cc4b7a37c2aba77ec808e8716470e3a8f925fee1
sagada <wlatmdqja@naver.com>
2021-12-30 23:49:22 +0900

    redis config

--- src/main/java/com/redis/wmp/redis/web/controller/AccountRepository.java
+++
@@ -1,6 +0,0 @@
-package com.redis.wmp.redis.web.controller;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AccountRepository extends JpaRepository<Account, Long> {
-}
